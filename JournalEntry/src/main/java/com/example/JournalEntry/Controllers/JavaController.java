0648c10f7fe4bdafc7450f827ad281feeb79659f
Chandu <chanduvalluri22@gmail.com>
2026-02-26 15:35:12 +0530

    5th commit

--- JournalEntry/src/main/java/com/example/JournalEntry/Controllers/JavaController.java
+++ JournalEntry/src/main/java/com/example/JournalEntry/Controllers/JavaController.java
@@ -8,6 +8,8 @@
     import org.springframework.beans.factory.annotation.Autowired;
     import org.springframework.http.HttpStatus;
     import org.springframework.http.ResponseEntity;
+    import org.springframework.security.core.Authentication;
+    import org.springframework.security.core.context.SecurityContextHolder;
     import org.springframework.transaction.annotation.Transactional;
     import org.springframework.web.bind.annotation.*;
 
@@ -28,17 +30,15 @@
 
 
             @GetMapping
-            public ResponseEntity<?> GetJournalEntriesofUser(@PathVariable String Username) {
-                User user = Userservice.GetUserbyusername(Username);
-                List <JournalEntity> L =  user.getJournalentries();
-                if (L != null && L.isEmpty()) {
-                    return new ResponseEntity<>(L, HttpStatus.FOUND);
+            public ResponseEntity<List<JournalEntity>> GetJournalEntriesofUser( ) {
+                List<JournalEntity> journalEntities = journalEntryService.GetJournalofauser();
+                if (journalEntities != null && !journalEntities.isEmpty()) {
+                    return ResponseEntity.ok(journalEntities);
                 }
-                    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-
+                return ResponseEntity.noContent().build();
             }
             @Transactional
-            @PostMapping("/{Username}")
+            @PostMapping
         public ResponseEntity <JournalEntity> PostEntries(@RequestBody JournalEntity E, @PathVariable String Username) {
                 try {
                     journalEntryService.SaveEntry(E, Username);
--- JournalEntry/src/main/java/com/example/JournalEntry/Service/JournalEntryService.java
+++ JournalEntry/src/main/java/com/example/JournalEntry/Service/JournalEntryService.java
@@ -6,6 +6,8 @@ import com.example.JournalEntry.Repository.JournalEntryRepo;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +21,6 @@ import java.util.Optional;
 
     @Autowired
     private UserEntryService UserService;
-
-
 
     public void  SaveEntry(JournalEntity JournalEntry, String Username) {
         User userinDB = UserService.GetUserbyusername(Username);
@@ -39,9 +39,16 @@ import java.util.Optional;
         return JournalEntryrepo.findAll();
     }
 
-    public List<JournalEntity> GetJournalofauser (String Username) {
-        User userfound = UserService.GetUserbyusername(Username);
-          return  userfound.getJournalentries();
+    public List<JournalEntity> GetJournalofauser ( ) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String name = authentication.getName();
+        User user = UserService.GetUserbyusername(name);
+        List<JournalEntity> journalentries = user.getJournalentries();
+        if (journalentries != null && !journalentries.isEmpty()) {
+            return journalentries;
+        }
+        return null;
+
     }
 
     public void DeleteEntry(ObjectId id, String Username) {
